4e9af9c9f09ee2713b09eb37e65233e853c53f7e
vassagothunder <vassagothunder@yahoo.com>
2016-01-27 17:55:29 +0300

    incapsulation

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,16 +10,16 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            point p1 = new point();
-            p1.x = 1;
+            point p1 = new point(1,3,'*');
+           /* p1.x = 1;
             p1.y = 3;
-            p1.sym = '*';
+            p1.sym = '*';*/
             p1.Draw();
 
-            point p2 = new point();
-            p2.x = 4;
+            point p2 = new point(4,5,'x');
+      /*      p2.x = 4;
             p2.y = 5;
-            p2.sym = 'x';
+            p2.sym = 'x';*/
             p2.Draw();
 
 
