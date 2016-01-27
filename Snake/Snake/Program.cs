cef96569106b3c00a28c5e0053fa2590e3c48640
vassagothunder <vassagothunder@yahoo.com>
2016-01-27 17:42:48 +0300

    class and method

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,27 +10,35 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            point p1 = new point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
+            point p2 = new point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = 'x';
+            p2.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = 'x';
 
-            Draw(x2, y2, sym2);
+            /*  int x1 = 1;
+              int y1 = 3;
+              char sym1 = '*';
+              Draw(x1, y1, sym1);
+
+              int x2 = 4;
+              int y2 = 5;
+              char sym2 = 'x';
+
+              Draw(x2, y2, sym2); */
 
             Console.ReadLine();
 
 
         }
 
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+  
     }
 }
